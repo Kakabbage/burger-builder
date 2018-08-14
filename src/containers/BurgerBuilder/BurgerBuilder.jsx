718f10adc29b741c385065569818175a44617d4b
Nghia <kakabbage@gmail.com>
2018-08-15 00:15:39 +0700

    Initialized Burger Component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Burger from '../../components/Burger/Burger';
+import Auxi from '../../hoc/Auxi';
+
+class BurgerBuilder extends Component {
+  render () {
+    return (
+      <Auxi>
+        <Burger />
+        <div>Build Control</div>
+      </Auxi>
+    );
+  }
+}
+
+export default BurgerBuilder;
